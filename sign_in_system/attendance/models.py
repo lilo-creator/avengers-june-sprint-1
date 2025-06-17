0f588f0057394849f5a8b8cd1bf9aea6f9b9ff85
lilo_creator <lmlilian702@gmail.com>
2025-06-17 15:01:32 +0300

    atendance forms and models

--- sign_in_system/attendance/models.py
+++ sign_in_system/attendance/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class Attendance(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     check_in = models.DateTimeField(null=True, blank=True)
     check_out = models.DateTimeField(null=True, blank=True)
 
